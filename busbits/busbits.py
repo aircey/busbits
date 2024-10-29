2be0a8bdad3805aa6248e1fcdf4f092bec860cc0
Romain Colombo <7079421+aircey@users.noreply.github.com>
2024-10-29 11:40:12 +0100

    Migrate schema into models

--- busbits/busbits.py
+++ busbits/busbits.py
@@ -1,27 +1,25 @@
 import yaml
 
 from busbits.models import Device
-from .yaml_schemas.device import DEVICE_SCHEMA
 
 
 def parse_busbits_yaml(file_path):
     with open(file_path, "r") as file:
         data = yaml.safe_load(file)
 
-    validated_data = DEVICE_SCHEMA(data)
-    return validated_data
+    return Device(data)
 
 
 def main():
     yaml_file_path = "./busbits/devices/testdevice/_root.yaml"
     try:
-        validated_data = parse_busbits_yaml(yaml_file_path)
+        device = parse_busbits_yaml(yaml_file_path)
         print("YAML parsed and validated successfully:")
+        print(device)
+
     except Exception as e:
         print(f"Validation error: {e}")
-
-    device = Device.from_dict(validated_data["device"])
-    print(device)
+        raise e
 
 
 if __name__ == "__main__":
--- busbits/models.py
+++ busbits/models.py
@@ -1,54 +1,144 @@
+from enum import Enum
 from typing import List, Optional, Dict
+import abc
+
+import voluptuous as vol
 
 
-class Range:
-    def __init__(self, offset: int, step: int, unit: str):
-        self.offset = offset
-        self.step = step
-        self.unit = unit
+class WithSchema(metaclass=abc.ABCMeta):
+    _SCHEMA: Optional[vol.Schema] = None
+    AUTO_PROPS: Dict = []
 
-    def __repr__(self):
-        return f"Range(offset={self.offset}, step={self.step}, unit={self.unit})"
+    validated_props: Dict = {}
+
+    def __init__(self, props: Dict) -> None:
+        self.validated_props = self.validate_schema(props)
+        for key in self.AUTO_PROPS:
+            if key not in self.validated_props:
+                raise ValueError(f"Missing required property: {key}")
+            setattr(self, key, self.validated_props[key])
 
     @classmethod
-    def from_dict(cls, data):
-        d = dict(data)
-        return cls(**d)
+    @abc.abstractmethod
+    def _build_schema(cls) -> vol.Schema:
+        pass
+
+    @classmethod
+    def validate_schema(cls, data) -> vol.Schema:
+        if cls._SCHEMA is None:
+            cls._SCHEMA = cls._build_schema()
+        return cls._SCHEMA(data)
+
+    @classmethod
+    def vol_isinstance(cls, obj):
+        if not isinstance(obj, cls):
+            raise ValueError
+        return obj
+
+    @classmethod
+    def vol_may_coerce(cls):
+        return vol.Any(cls.vol_isinstance, vol.Coerce(cls))
+
+    @classmethod
+    def vol_may_coerce_nullable(cls):
+        return vol.Any(cls.vol_may_coerce(), None)
 
 
-class Values:
-    def __init__(
-        self,
-        range: Optional[Range] = None,
-        enum: Optional[Dict[str, int]] = None,
-        boolean: Optional[Dict[bool, int]] = None,
-    ):
-        self.range = range
-        self.enum = enum
-        self.boolean = boolean
+class Access(Enum):
+    READ = "r"
+    WRITE = "w"
+    READ_WRITE = "rw"
+
+
+class ValuesHolder(metaclass=abc.ABCMeta):
+    @property
+    @abc.abstractmethod
+    def size(self) -> int:
+        pass
+
+
+class Range(WithSchema):
+    values: "Values"
+
+    AUTO_PROPS = ["offset", "step", "unit"]
+    offset: int
+    step: int
+    unit: str
+
+    @classmethod
+    def _build_schema(cls) -> vol.Schema:
+        return vol.Schema(
+            {
+                vol.Required("offset"): int,
+                vol.Required("step"): int,
+                vol.Required("unit"): str,
+            }
+        )
+
+    def __init__(self, props: Dict):
+        super().__init__(props)
+
+    def __repr__(self) -> str:
+        return f"Range(offset={self.offset}, step={self.step}, unit={self.unit})"
+
+
+class Values(WithSchema):
+    value_holder: ValuesHolder
+
+    AUTO_PROPS = ["range", "enum", "boolean"]
+    range: Optional[Range]
+    enum: Optional[Dict[str, int]]
+    boolean: Optional[Dict[bool, int]]
+
+    @classmethod
+    def _build_schema(cls) -> vol.Schema:
+        return vol.Schema(
+            {
+                vol.Optional("range", default=None): Range.vol_may_coerce_nullable(),
+                # TODO: Enum and boolean should be objects
+                vol.Optional("enum", default=None): vol.Any({str: int}, None),
+                vol.Optional("boolean", default=None): vol.Any({bool: int}, None),
+            }
+        )
+
+    def __init__(self, props: Dict):
+        super().__init__(props)
+
+        if self.range is not None:
+            self.range.values = self
 
     def __repr__(self):
         return f"Values(range={self.range}, enum={self.enum}, boolean={self.boolean})"
 
+
+class Field(ValuesHolder, WithSchema):
+    register: "Register"
+
+    AUTO_PROPS = ["name", "bit_offset", "bit_length", "values"]
+    name: str
+    bit_offset: int
+    bit_length: int
+    values: Values
+
     @classmethod
-    def from_dict(cls, data):
-        d = dict(data)
-        d["range"] = Range.from_dict(d.get("range")) if d.get("range") else None
-        return cls(**d)
+    def _build_schema(cls) -> vol.Schema:
+        return vol.Schema(
+            {
+                vol.Required("name"): str,
+                vol.Required("bit_offset"): vol.All(int, vol.Range(min=0)),
+                vol.Required("bit_length"): vol.All(int, vol.Range(min=1)),
+                vol.Optional("values", default={}): Values.vol_may_coerce(),
+            }
+        )
 
+    def __init__(self, props: Dict):
+        super().__init__(props)
 
-class Field:
-    def __init__(
-        self,
-        name: str,
-        bit_offset: int,
-        bit_length: int,
-        values: Optional[Values] = None,
-    ):
-        self.name = name
-        self.bit_offset = bit_offset
-        self.bit_length = bit_length
-        self.values = values
+        self.values.value_holder = self
+
+    @property
+    def size(self) -> int:
+        return self.bit_length
 
     def __repr__(self):
         return (
@@ -56,29 +146,36 @@ class Field:
             f"bit_length={self.bit_length}, values={self.values},)"
         )
 
+
+class Register(WithSchema):
+    device: "Device"
+
+    AUTO_PROPS = ["name", "address", "size", "access", "description", "fields"]
+    name: str
+    address: int
+    size: int
+    access: Access
+    description: str
+    fields: List[Field]
+
     @classmethod
-    def from_dict(cls, data):
-        d = dict(data)
-        d["values"] = Values.from_dict(d.get("values")) if d.get("values") else None
-        return cls(**d)
+    def _build_schema(cls) -> vol.Schema:
+        return vol.Schema(
+            {
+                vol.Required("name"): str,
+                vol.Required("address"): int,
+                vol.Required("size"): vol.All(int, vol.Range(min=1)),
+                vol.Required("access"): vol.Coerce(Access),
+                vol.Required("description"): str,
+                vol.Optional("fields", default=[]): [Field.vol_may_coerce()],
+            }
+        )
 
+    def __init__(self, props: Dict):
+        super().__init__(props)
 
-class Register:
-    def __init__(
-        self,
-        name: str,
-        address: int,
-        size: int,
-        access: str,
-        description: str,
-        fields: Optional[List[Field]] = None,
-    ):
-        self.name = name
-        self.address = address
-        self.size = size
-        self.access = access
-        self.description = description
-        self.fields = fields or []
+        for field in self.fields:
+            field.register = self
 
     def __repr__(self):
         return (
@@ -87,23 +184,34 @@ class Register:
             f"fields={self.fields})"
         )
 
+
+class CommandParameter(ValuesHolder, WithSchema):
+    command: "Command"
+
+    AUTO_PROPS = ["name", "description", "values"]
+    name: str
+    description: str
+    values: Values
+
     @classmethod
-    def from_dict(cls, data):
-        d = dict(data)
-        d["fields"] = [Field.from_dict(field) for field in d.get("fields", [])]
-        return cls(**d)
+    def _build_schema(cls) -> vol.Schema:
+        return vol.Schema(
+            {
+                vol.Required("name"): str,
+                vol.Required("description"): str,
+                vol.Optional("values", default={}): Values.vol_may_coerce(),
+            }
+        )
 
+    def __init__(self, props: Dict):
+        super().__init__(props)
 
-class CommandParameter:
-    def __init__(
-        self,
-        name: str,
-        description: str,
-        values: Optional[Values] = None,
-    ):
-        self.name = name
-        self.description = description
-        self.values = values
+        self.values.value_holder = self
+
+    @property
+    def size(self) -> int:
+        # TODO: Implement size calculation
+        return 0
 
     def __repr__(self):
         return (
@@ -111,25 +219,34 @@ class CommandParameter:
             f"values={self.values})"
         )
 
+
+class Command(WithSchema):
+    device: "Device"
+
+    AUTO_PROPS = ["name", "command_code", "description", "parameters"]
+    name: str
+    command_code: int
+    description: str
+    parameters: List[CommandParameter]
+
     @classmethod
-    def from_dict(cls, data):
-        d = dict(data)
-        d["values"] = Values.from_dict(d.get("values")) if d.get("values") else None
-        return cls(**d)
+    def _build_schema(cls) -> vol.Schema:
+        return vol.Schema(
+            {
+                vol.Required("name"): str,
+                vol.Required("command_code"): int,
+                vol.Required("description"): str,
+                vol.Optional("parameters", default=[]): [
+                    CommandParameter.vol_may_coerce()
+                ],
+            }
+        )
 
+    def __init__(self, props: Dict):
+        super().__init__(props)
 
-class Command:
-    def __init__(
-        self,
-        name: str,
-        command_code: int,
-        description: str,
-        parameters: Optional[List[CommandParameter]] = None,
-    ):
-        self.name = name
-        self.command_code = command_code
-        self.description = description
-        self.parameters = parameters or []
+        for param in self.parameters:
+            param.command = self
 
     def __repr__(self):
         return (
@@ -137,37 +254,36 @@ class Command:
             f"description={self.description}, parameters={self.parameters})"
         )
 
+
+class Device(WithSchema):
+    AUTO_PROPS = ["name", "description", "registers", "commands"]
+    name: str
+    description: str
+    registers: List[Register]
+    commands: List[Command]
+
     @classmethod
-    def from_dict(cls, data):
-        d = dict(data)
-        d["parameters"] = [
-            CommandParameter.from_dict(param) for param in d.get("parameters", [])
-        ]
-        return cls(**d)
+    def _build_schema(cls) -> vol.Schema:
+        return vol.Schema(
+            {
+                vol.Required("name"): str,
+                vol.Required("description"): str,
+                vol.Optional("registers", default=[]): [Register.vol_may_coerce()],
+                vol.Optional("commands", default=[]): [Command.vol_may_coerce()],
+            }
+        )
 
+    def __init__(self, props: Dict):
+        super().__init__(props)
 
-class Device:
-    def __init__(
-        self,
-        name: str,
-        description: str,
-        registers: Optional[List[Register]] = None,
-        commands: Optional[List[Command]] = None,
-    ):
-        self.name = name
-        self.description = description
-        self.registers = registers or []
-        self.commands = commands or []
+        for reg in self.registers:
+            reg.device = self
+
+        for cmd in self.commands:
+            cmd.device = self
 
     def __repr__(self):
         return (
             f"Device(name={self.name}, description={self.description},"
             f"registers={self.registers}, commands={self.commands})"
         )
-
-    @classmethod
-    def from_dict(cls, data):
-        d = dict(data)
-        d["registers"] = [Register.from_dict(reg) for reg in d.get("registers", [])]
-        d["commands"] = [Command.from_dict(cmd) for cmd in d.get("commands", [])]
-        return cls(**d)
