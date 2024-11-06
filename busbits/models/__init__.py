e19f29607865efb6ac8fcc69662fc86d8b5aa0c2
Romain Colombo <7079421+aircey@users.noreply.github.com>
2024-11-06 16:33:20 +0100

    Fix WithSchema typing and exceptions

--- busbits/models/__init__.py
+++ busbits/models/__init__.py
@@ -1,5 +1,5 @@
 import abc
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
 import voluptuous as vol
 
@@ -9,7 +9,7 @@ DOMAIN_ONLY_ENTITY = "_domain_only"
 
 class WithSchema(metaclass=abc.ABCMeta):
     _SCHEMA: Optional[vol.Schema] = None
-    AUTO_PROPS: Dict = []
+    AUTO_PROPS: List = []
 
     validated_props: Dict = {}
 
@@ -18,7 +18,7 @@ class WithSchema(metaclass=abc.ABCMeta):
         self.validated_props = cls.validate_schema(props)
         for key in cls.AUTO_PROPS:
             if key not in self.validated_props:
-                raise ValueError(f"Missing required property: {key}")
+                raise Exception(f"Missing required property: {key}")
             setattr(self, key, self.validated_props[key])
 
     @classmethod
@@ -27,7 +27,7 @@ class WithSchema(metaclass=abc.ABCMeta):
         pass
 
     @classmethod
-    def validate_schema(cls, data) -> vol.Schema:
+    def validate_schema(cls, data) -> Dict:
         if cls._SCHEMA is None:
             cls._SCHEMA = cls._build_schema()
         return cls._SCHEMA(data)
